664872e1fb8292837d60a63823f3bf72dfad38b5
Jorge-E-Guerrero <52971305+Jorge-E-Guerrero@users.noreply.github.com>
2022-03-06 11:17:43 -0600

    Spring boot update/delete example

--- BEVentas/src/main/java/net/codejava/Controlador/PruebaControlador.java
+++ BEVentas/src/main/java/net/codejava/Controlador/PruebaControlador.java
@@ -1,8 +1,7 @@
 package net.codejava.Controlador;
 
-import java.util.List;
+import java.util.Optional;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,12 +20,15 @@ public class PruebaControlador {
 	@Autowired
 	private RepositorioPrueba repositorioPrueba;
 	
+	@GetMapping("/prueba")
+	public @ResponseBody Iterable<Prueba> getAllPruebas(){
+		return repositorioPrueba.findAll();
+	}
+	
 	
 	@PostMapping("/insertar")
 	public @ResponseBody String insertarPrueba (@RequestParam int nValor1, @RequestParam String nValor2) {
 	
-
-		
 		Prueba n = new Prueba(4,nValor1,nValor2);
 		//n.setId_prueba(4);
 		//n.setValor_1(nValor1);
@@ -36,10 +38,35 @@ public class PruebaControlador {
 		return "ok";
 	}
 	
-	
-	@GetMapping("/prueba")
-	public @ResponseBody Iterable<Prueba> getAllPruebas(){
-		return repositorioPrueba.findAll();
+	@PostMapping("/modificar")
+	public @ResponseBody String ModificarPrueba (@RequestParam int nId, @RequestParam int nValor1, @RequestParam String nValor2) {
+		
+		Optional<Prueba> n = repositorioPrueba.findById(nId);
+		Prueba _n = n.get();
+		_n.setValor_1(nValor1);
+		_n.setValor_2(nValor2);
+		repositorioPrueba.save(_n);
+		
+		return "Se ha modificado correctamente";
 	}
 	
+	@PostMapping("/borrar")
+	public @ResponseBody String BorrarPrueba (@RequestParam int nId) {
+	
+		if(repositorioPrueba.existsById(nId)) {
+			
+			repositorioPrueba.deleteById(nId);
+			
+			if(repositorioPrueba.existsById(nId)) {
+				return "No se ha logrado borrar el usuario";
+			} 
+			else 
+			{
+				return "Se ha borrado correctamente";
+			}
+			
+		} else {
+			return "El usuario no existe";
+		}		
+	}
 }
