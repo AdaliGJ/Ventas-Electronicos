54430cdf4db3b06f9ed5f13d454386356c5d5582
javierlopezg00 <javierlopezguzman00@gmail.com>
2022-03-06 20:08:25 -0600

    controlador marcas

---
+++ BEVentas/src/main/java/net/codejava/Controlador/MarcasControlador.java
@@ -0,0 +1,12 @@
+package net.codejava.Controlador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.codejava.Repositorio.RepositorioPrueba;
+
+@RestController 
+public class MarcasControlador {
+	
+	@Autowired
+	private marcaServiceApi marcaServiceApi;
+}
---
+++ BEVentas/src/main/java/net/codejava/Repositorio/MarcaServiceApi.java
@@ -0,0 +1,13 @@
+package net.codejava.Repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+
+import net.codejava.Entidad.Prueba;
+
+
+
+public interface MarcaServiceApi extends CrudRepository<Prueba, Integer>{
+	
+
+
+}
