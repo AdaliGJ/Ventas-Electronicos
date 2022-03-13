173633ca806e9e3f09c3639594b79138b28994d1
javierlopezg00 <javierlopezguzman00@gmail.com>
2022-03-13 14:19:32 -0600

    marcas

--- feventas/src/components/Marcas/marcas.js
+++ feventas/src/components/Marcas/marcas.js
@@ -1,26 +1,68 @@
-import React, {Component} from 'react';
-/*import { marcaService } from '../../service/marcaService';*/
+import React, {useRef, useContext, useState} from 'react';
+import {LoginContext} from "../../context/LoginContext.js";
+import { Link, useHistory} from "react-router-dom";
+import Alert from '@material-ui/lab/Alert';
+import './marcas.css'
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import LockIcon from '@material-ui/icons/Lock';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 
-class  Marcas extends React.Component {
-    constructor(){
-        super();
-        this.state = {};
 
-        /*this.marcaService = new marcaService();*/
-    }
 
-    componentDidMount(){
-       /* this.marcaService.getAll().then(data => {
-            console.log(data);
-        })*/
+function Marcas(props){
 
-        console.log("Hola mundo");
-    }
+    const { setUsername, username, tipoUsuario, setTipoUsuario }=useContext(LoginContext);
+    const [msg, setMsg]=useState('');
+    const[error, setError]=useState(false);
+        
+    const usuarioRef = useRef(null);
+    const contRef = useRef(null);
 
-    render(){
+    const history = useHistory();
+
+       const handleLogin=()=>{
+                
+
+               
+                /*console.log(username);
+                console.log(tipoUsuario);*/
+                console.log("Hola");
+
+        }
+
+
         return(
-            <h1>Hola mundo</h1>
+            <div className="page">
+                    <h1 id="login_tit">Registro Marcas</h1>
+                    <Grid container direction={"column"} spacing={7}>
+                        <Grid item>
+                            <TextField className="standard-basic" label="Marca" placeholder="Marca" InputProps={{
+                                startAdornment: (
+                                  <InputAdornment position="start">
+                                    <AccountCircleIcon />
+                                  </InputAdornment>
+                                ),
+                              }} inputRef={usuarioRef}/>
+                        </Grid>
+                        
+                    </Grid>
+                    <Grid container direction={"column"} spacing={8}>
+                        <Grid item id={error ? "error": "noerror"}>
+                            <Alert severity="error">Error</Alert>
+                        </Grid>
+                        <Grid item>
+                            <Button id="enviar" variant="contained" onClick={handleLogin}>Registrar Marca</Button>
+                        </Grid>
+                    </Grid>
+            </div>
         );
-    }
+
 }
+
 export default Marcas;
