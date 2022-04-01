bf07f03429d74a974f9571e962d4c6f01fd97829
Jorge-E-Guerrero <52971305+Jorge-E-Guerrero@users.noreply.github.com>
2022-04-01 14:20:34 -0600

    Java doc

--- BEVentas/src/main/java/net/codejava/Controlador/Dispositivos_individualesControlador.java
+++ BEVentas/src/main/java/net/codejava/Controlador/Dispositivos_individualesControlador.java
@@ -25,18 +25,36 @@ public class Dispositivos_individualesControlador {
 	@Autowired
 	private RepositorioDispositivos_individuales repositorioDispositivosIndividuales;
 	
+	
+	
+	
+	/**
+	 * Query para obtener todos los dispositivos en la base de datos
+	 * 
+	 * @return Lista de todos los dispositivos individuales en formato Json
+	 */
 	@GetMapping("/ObtenerTodos")
 	public @ResponseBody Iterable<Dispositivos_individuales> getAll(){
 		return repositorioDispositivosIndividuales.findAll();
 	}
 	
-	
+	/**
+	 * Query para obtener el registro de un solo dispositivo individual por medio de su numero de serie
+	 * 
+	 * @param nSerial Consta de un string de 20 digitos el cual corresponde a un numero serial del dispositvo en la base de datos
+	 * @return Al ingresar un numero serial valido, devuelve el registro correspondiente a ese dispositivo en formato Json
+	 */
 	@GetMapping("/Obtener")
 	public @ResponseBody Optional<Dispositivos_individuales> getOne(@RequestParam String nSerial){
 		return repositorioDispositivosIndividuales.findBySerie(nSerial);
 	}
 	
-	
+	/**
+	 * Query iterativo que se va a encargar de registrar nuevos dispositivos individuales en la base de datos
+	 * @param nIdInventario Es el numero de inventario del cual corresponde al nuevo dispositivo ingresado en la base de datos
+	 * @param nCantidad Cantidad de dispositivos que se desean ingresar en la base de datos
+	 * @return Devuelve todos los dipositivos generados con su numero de serie en un Json
+	 */
 	@PostMapping("/Registrar")
 	public @ResponseBody List<Dispositivos_individuales> registrar(@RequestParam int nIdInventario, @RequestParam int nCantidad) {
 	
