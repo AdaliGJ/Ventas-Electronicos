f0db10053869ce7e4db6c989de10688177a5d8ef
Jorge-E-Guerrero <guerrero191096@unis.edu.gt>
2022-04-15 12:58:06 -0600

    BE Unit Testing add #3

--- BEVentas/src/main/java/net/codejava/Entidad/VistaImgs.java
+++ BEVentas/src/main/java/net/codejava/Entidad/VistaImgs.java
@@ -28,28 +28,71 @@ public class VistaImgs {
 	private String descripcion;
 	@Column 
 	private String imagen;
+	
+	
+	public VistaImgs() {}
+	
+
 	public String getTipodispositivo() {
 		return tipodispositivo;
+	}
+	public void setTipodispositivo(String tipodispositivo) {
+		this.tipodispositivo = tipodispositivo;
 	}
 	public String getNombremarca() {
 		return nombremarca;
 	}
+	public void setNombremarca(String nombremarca) {
+		this.nombremarca = nombremarca;
+	}
 	public float getPreciolista() {
 		return preciolista;
+	}
+	public void setPreciolista(float preciolista) {
+		this.preciolista = preciolista;
 	}
 	public String getModelo() {
 		return modelo;
 	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
 	public int getIdinventario() {
 		return idinventario;
+	}
+	public void setIdinventario(int idinventario) {
+		this.idinventario = idinventario;
 	}
 	public String getDescripcion() {
 		return descripcion;
 	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 	public String getImagen() {
 		return imagen;
 	}
-	
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+
+	public VistaImgs(String tipodispositivo, String nombremarca, float preciolista, String modelo, int idinventario,
+			String descripcion, String imagen) {
+		super();
+		this.tipodispositivo = tipodispositivo;
+		this.nombremarca = nombremarca;
+		this.preciolista = preciolista;
+		this.modelo = modelo;
+		this.idinventario = idinventario;
+		this.descripcion = descripcion;
+		this.imagen = imagen;
+	}
+
+
+
+
+
 	@Override
 	public String toString() {
 		return "VistaImgs [tipodispositivo=" + tipodispositivo + ", nombremarca=" + nombremarca + ", preciolista="
--- BEVentas/src/main/java/net/codejava/Entidad/VistaInfoCliente.java
+++ BEVentas/src/main/java/net/codejava/Entidad/VistaInfoCliente.java
@@ -28,25 +28,63 @@ public class VistaInfoCliente {
 	private String nombrecliente;
 	
 	
+	
+	
+	
+	
+	
 	public int getNit() {
 		return nit;
+	}
+	public void setNit(int nit) {
+		this.nit = nit;
 	}
 	public int getTipocliente() {
 		return tipocliente;
 	}
+
+
+	public VistaInfoCliente() {}
+
+
+	public void setTipocliente(int tipocliente) {
+		this.tipocliente = tipocliente;
+	}
 	public String getNombretipocliente() {
 		return nombretipocliente;
+	}
+	public void setNombretipocliente(String nombretipocliente) {
+		this.nombretipocliente = nombretipocliente;
 	}
 	public String getPatente() {
 		return patente;
 	}
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
 	public String getVencimiento() {
 		return vencimiento;
+	}
+	public void setVencimiento(String vencimiento) {
+		this.vencimiento = vencimiento;
 	}
 	public String getNombrecliente() {
 		return nombrecliente;
 	}
-	
+	public void setNombrecliente(String nombrecliente) {
+		this.nombrecliente = nombrecliente;
+	}
+
+	public VistaInfoCliente(int nit, int tipocliente, String nombretipocliente, String patente, String vencimiento,
+			String nombrecliente) {
+		super();
+		this.nit = nit;
+		this.tipocliente = tipocliente;
+		this.nombretipocliente = nombretipocliente;
+		this.patente = patente;
+		this.vencimiento = vencimiento;
+		this.nombrecliente = nombrecliente;
+	}
 	
 	@Override
 	public String toString() {
