216f7e6b15a4a27b807666fa1633c23aab298bbf
Jorge-E-Guerrero <guerrero191096@unis.edu.gt>
2022-04-19 13:26:52 -0600

    BE javadoc Repository example

--- BEVentas/src/main/java/net/codejava/Repositorio/RepositorioDispositivos_individuales.java
+++ BEVentas/src/main/java/net/codejava/Repositorio/RepositorioDispositivos_individuales.java
@@ -6,6 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 
 import net.codejava.Entidad.Dispositivos_individuales;
 
+/**
+ * Esta Interfaz permite agregar funcionalidades CRUD a la entidad Dispositivos_individuales permitiendo definir tambien sus propios querys hacia la DB de Oracle por medio de la sintaxis que define JDBC
+ *
+ */
 public interface RepositorioDispositivos_individuales extends CrudRepository<Dispositivos_individuales, Integer>{
 	Optional<Dispositivos_individuales> findBySerie(String serie_dispositivo);
 	Iterable<Dispositivos_individuales> findByVendido(int vendido);
